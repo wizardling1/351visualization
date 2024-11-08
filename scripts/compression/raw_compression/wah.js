4f6a9415b0f4d23580a0b794712d056f75748bce
Daniel Basarab <dantramp52@gmail.com>
2024-11-08 14:16:07 -0800

    merge wahWithStates.js into wah.js returns states when returnStates flag is set

--- scripts/compression/raw_compression/wah.js
+++ scripts/compression/raw_compression/wah.js
@@ -45,77 +45,158 @@ const getWahCompression = (numRuns, runOf, chunkarr, wordSize, compressed, index
 
 }
 
-export const wahCompress = (string, wordSize) => {
-    let numChunks = Math.ceil(string.length / (wordSize - 1))
-    let index = 0
+const asUnsigned = (num, wordSize) => typeof num === "bigint" ? BigInt.asUintN(wordSize, num) : num >>> 0;
 
-    let compressed = getTypedArray(wordSize, numChunks)
-    const parse = getParse(wordSize, wordSize - 1)
+export const wahCompress = (string, wordSize, returnStates = false) => {
+    let numChunks = Math.ceil(string.length / (wordSize - 1));
+    let index = 0;
+
+    let compressed = getTypedArray(wordSize, numChunks);
+    const parse = getParse(wordSize, wordSize - 1);
     const cast = getCast(wordSize);
 
-    let runOnes = cast(0)
-    let runZeros = cast(0)
-    let chunkSize = wordSize - 1
-    let onesNum = (cast(1 >>> 0) << cast(chunkSize)) - cast(1)
-    let maxRunSize = (cast(1) << cast(wordSize - 2)) - cast(1)
-    let lastchunkflg = 0
+    let runOnes = cast(0);
+    let runZeros = cast(0);
+    let chunkSize = wordSize - 1;
+    let onesNum = (cast(1 >>> 0) << cast(chunkSize)) - cast(1);
+    let maxRunSize = (cast(1) << cast(wordSize - 2)) - cast(1);
+    let lastchunkflg = 0;
+
+    let states = [];
+    let currentStartIndex = 0;
 
     for (let i = 0; i < string.length; i += chunkSize) {
         let chunkStr = string.slice(i, i + chunkSize);
         let chunk = parse(chunkStr);
-        // make >= to just be > instead
         if (i + chunkSize > string.length) {
-            lastchunkflg = 1
+            lastchunkflg = 1;
         }
 
         if (chunk == 0 && lastchunkflg == 0) {
             if (runOnes > 0) { //RUN OF 1's ended, run of 0's started
-                getWahCompression(runOnes, 1, null, wordSize, compressed, index++, cast)
-                runOnes = cast(0)
+                getWahCompression(runOnes, 1, null, wordSize, compressed, index++, cast);
+                if (returnStates) {
+                    states.push({
+                        runs: Number(runOnes),
+                        runType: '1',
+                        startIndex: currentStartIndex,
+                        compressed: asUnsigned(compressed[index - 1], wordSize).toString(2).padStart(wordSize, '0'),
+                    });
+                }
+                runOnes = cast(0);
+                currentStartIndex = i;
             }
-            runZeros++  //"RUN OF 0's
+            if (runZeros == 0) {
+                currentStartIndex = i;
+            }
+            runZeros++;  //"RUN OF 0's
             if (runZeros >= maxRunSize) {
-                getWahCompression(runZeros, 0, null, wordSize, compressed, index++, cast)
-                runZeros = cast(0)
+                getWahCompression(runZeros, 0, null, wordSize, compressed, index++, cast);
+                if (returnStates) {
+                    states.push({
+                        runs: Number(runZeros),
+                        runType: '0',
+                        startIndex: currentStartIndex,
+                        compressed: asUnsigned(compressed[index - 1], wordSize).toString(2).padStart(wordSize, '0'),
+                    });
+                }
+                runZeros = cast(0);
+                currentStartIndex = i + chunkSize;
             }
-        }
-        // Comparison here is (chunk ^ onesNum) == 0 instead of chunk == onesNum because with a word size of 32,
-        // JS sometimes may interpret chunk as an unsigned int and onesNum as a signed int which breaks == comparison.
-        else if ((chunk ^ onesNum) == 0 && lastchunkflg == 0) {//RUN OF 1's
+        } else if ((chunk ^ onesNum) == 0 && lastchunkflg == 0) {//RUN OF 1's
             if (runZeros > 0) {
-                getWahCompression(runZeros, 0, null, wordSize, compressed, index++, cast)
-                runZeros = cast(0)
+                getWahCompression(runZeros, 0, null, wordSize, compressed, index++, cast);
+                if (returnStates) {
+                    states.push({
+                        runs: Number(runZeros),
+                        runType: '0',
+                        startIndex: currentStartIndex,
+                        compressed: asUnsigned(compressed[index - 1], wordSize).toString(2).padStart(wordSize, '0'),
+                    });
+                }
+                runZeros = cast(0);
+                currentStartIndex = i;
             }
-            runOnes++
+            if (runOnes == 0) {
+                currentStartIndex = i;
+            }
+            runOnes++;
             if (runOnes >= maxRunSize) { //RUN OF 1's ended
-                getWahCompression(runOnes, 1, null, wordSize, compressed, index++, cast)
-                runOnes = cast(0)
+                getWahCompression(runOnes, 1, null, wordSize, compressed, index++, cast);
+                if (returnStates) {
+                    states.push({
+                        runs: Number(runOnes),
+                        runType: '1',
+                        startIndex: currentStartIndex,
+                        compressed: asUnsigned(compressed[index - 1], wordSize).toString(2).padStart(wordSize, '0'),
+                    });
+                }
+                runOnes = cast(0);
+                currentStartIndex = i + chunkSize;
             }
-        }
-        else {
+        } else {
             if (runOnes > 0) {//encode run of 1s first
-                getWahCompression(runOnes, 1, null, wordSize, compressed, index++, cast)
-                runOnes = cast(0)
-            }
-            else if (runZeros > 0) {//encode run of 0s first
-                getWahCompression(runZeros, 0, null, wordSize, compressed, index++, cast)
-                runZeros = cast(0)
+                getWahCompression(runOnes, 1, null, wordSize, compressed, index++, cast);
+                if (returnStates) {
+                    states.push({
+                        runs: Number(runOnes),
+                        runType: '1',
+                        startIndex: currentStartIndex,
+                        compressed: asUnsigned(compressed[index - 1], wordSize).toString(2).padStart(wordSize, '0'),
+                    });
+                }
+                runOnes = cast(0);
+            } else if (runZeros > 0) {//encode run of 0s first
+                getWahCompression(runZeros, 0, null, wordSize, compressed, index++, cast);
+                if (returnStates) {
+                    states.push({
+                        runs: Number(runZeros),
+                        runType: '0',
+                        startIndex: currentStartIndex,
+                        compressed: asUnsigned(compressed[index - 1], wordSize).toString(2).padStart(wordSize, '0'),
+                    });
+                }
+                runZeros = cast(0);
             }
             // encode Literal
-            getWahCompression(0, 0, chunk, wordSize, compressed, index++, cast)
+            getWahCompression(0, 0, chunk, wordSize, compressed, index++, cast);
+            if (returnStates) {
+                states.push({
+                    runs: 0,
+                    runType: '',
+                    startIndex: i,
+                    compressed: asUnsigned(compressed[index - 1], wordSize).toString(2).padStart(wordSize, '0'),
+                });
+            }
+            currentStartIndex = i + chunkSize;
         }
     }
 
     if (runOnes > 0) {//encode run of 1"
-        getWahCompression(runOnes, 1, null, wordSize, compressed, index++, cast)
-    }
-    else if (runZeros > 0) {//encode run of 0
-        getWahCompression(runZeros, 0, null, wordSize, compressed, index++, cast)
+        getWahCompression(runOnes, 1, null, wordSize, compressed, index++, cast);
+        if (returnStates) {
+            states.push({
+                runs: Number(runOnes),
+                runType: '1',
+                startIndex: currentStartIndex,
+                compressed: asUnsigned(compressed[index - 1], wordSize).toString(2).padStart(wordSize, '0'),
+            });
+        }
+    } else if (runZeros > 0) {//encode run of 0
+        getWahCompression(runZeros, 0, null, wordSize, compressed, index++, cast);
+        if (returnStates) {
+            states.push({
+                runs: Number(runZeros),
+                runType: '0',
+                startIndex: currentStartIndex,
+                compressed: asUnsigned(compressed[index - 1], wordSize).toString(2).padStart(wordSize, '0'),
+            });
+        }
     }
 
-    return {
+    return returnStates ? states : {
         compressed: compressed,
         length: index,
         str: bitsToString(compressed, index, wordSize),
-    }
+    };
 }
